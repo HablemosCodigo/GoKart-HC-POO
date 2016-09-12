a304124f77b60c6e326c83c05828e5790bc08272
Gabriela Reyes <Gabriela Reyes>
2016-09-11 22:12:04 -0500

    Constructores

--- src/com/company/GoKart.java
+++ src/com/company/GoKart.java
@@ -3,8 +3,11 @@ package com.company;
 public class GoKart {
 
     //Se le asigna la letra m al inicio para indicar que es una Variable Miembro
-
     private String mColor = "Rojo";
+
+    public GoKart(String color){
+        mColor = color;
+    }
 
     public String getColor(){
         return mColor;
