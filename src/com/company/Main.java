0b18d63a78ec2b33bb7537c550d3df99fe1c6870
Gabriela Reyes <Gabriela Reyes>
2016-09-11 22:24:43 -0500

    Imprimir el color

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Creemos un Go Kart!!");
+        System.out.println("Creemos un Go Kart!!\n");
 
         //Toma un solo parametro
         GoKart goKart = new GoKart("Azul");
+        System.out.println("El Go Kart es de color: " + goKart.getColor());
     }
 }
