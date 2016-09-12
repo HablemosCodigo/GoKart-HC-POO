8a087fcb6b2dae832886f11def1d2877433913ee
Gabriela Reyes <Gabriela Reyes>
2016-09-11 22:22:33 -0500

    Creacion de objetos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Creemos un Go Kart!!");
+
+        //Toma un solo parametro
+        GoKart goKart = new GoKart("Azul");
     }
 }
